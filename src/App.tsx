70847eba7df6071b85730b81dc3f140aa7d29da7
kamczill <kam134@interia.eu>
2023-09-02 14:50:19 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col justify-center align-center w-full'>
+        chuj
+      </div>
+    </>
+  )
+}
+
+export default App
